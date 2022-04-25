94a63c344c9041c85ff7590e2bd8c6a72dd5e107
tirimid <q44q44q44q44@gmail.com>
2022-04-25 19:50:27 +0400

    discard my last commit please

--- boot/src/libc/string.c
+++ boot/src/libc/string.c
@@ -3,9 +3,7 @@
 void *
 memset(void *dst, uint8_t b, size_t size)
 {
-        size_t i;
-
-        for (i = 0; i < size; ++i)
+        for (size_t i = 0; i < size; ++i)
                 *((uint8_t *)dst + i) = b;
         return dst;
 }
@@ -13,9 +11,8 @@ memset(void *dst, uint8_t b, size_t size)
 wchar_t *
 wcscpy(wchar_t *restrict dst, const wchar_t *restrict src)
 {
-        int i;
-
-        for (i = 0; src[i] != L'\0'; ++i)
+        int i = 0;
+        for (; src[i] != L'\0'; ++i)
                 dst[i] = src[i];
         dst[i] = L'\0';
         return dst;
@@ -24,12 +21,11 @@ wcscpy(wchar_t *restrict dst, const wchar_t *restrict src)
 int
 memcmp(const uint8_t *restrict a, const uint8_t *restrict b, size_t size)
 {
-        size_t i;
-
-        for (i = 0; i < size; ++i) {
-                if (*a[i] > *b[i])
+        for (size_t i = 0; i < size; ++i) {
+                if (a[i] > b[i])
                         return 1;
-                else if (*a[i] < *b[i])
+                else if (a[i] < b[i])
                         return -1;
         }
+        return 0;
 }
