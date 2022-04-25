3e816a672c9ab4470f528d92527bbe05e334167a
dmfedorin <m7996@students.eischools.ae>
2022-04-25 12:18:55 +0400

    began work on elf loader

--- boot/src/main.c
+++ boot/src/main.c
@@ -4,15 +4,18 @@
 #include "io/files.h"
 #include "io/text.h"
 #include "mem/pool.h"
+#include "load/elf.h"
 
 static void
-print_file_conts(EFI_HANDLE img_handle, const wchar_t *file_name)
+test_elf_file(EFI_HANDLE img_handle, const wchar_t *file_name)
 {
         EFI_FILE_HANDLE vol = get_image_volume(img_handle);
         EFI_FILE_HANDLE file = open_file(vol, file_name);
         wchar_t *file_buf = alloc_pool_mem(file_size(file));
         read_file(file, file_buf, file_size(file));
-        print_line(file_buf);
+        const struct elf_header *hdr = (const struct elf_header *)file_buf;
+        if (hdr->inst_set == ELF_HEADER_INST_SET_X86_64)
+                log_info(L"x86-64 elf file");
         free_pool_mem(file_buf);
         close_file(file);
 }
@@ -21,7 +24,7 @@ EFI_STATUS
 efi_main(EFI_HANDLE img_handle, EFI_SYSTEM_TABLE *sys_table)
 {
         init_boot(sys_table);
-        print_file_conts(img_handle, L"uefi\\test.txt");
+        test_elf_file(img_handle, L"sys\\tcf-kernel.elf");
         log_error(L"success, hanging program");
         return EFI_SUCCESS;
 }
