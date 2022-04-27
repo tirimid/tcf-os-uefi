d15ae71b598efe2465dc64f0296d6013c335fa7f
dmfedorin <m7996@students.eischools.ae>
2022-04-27 12:18:58 +0400

    progress on elf loader

---
+++ boot/src/mem/paging.c
@@ -0,0 +1,23 @@
+#include "mem/paging.h"
+
+void *
+alloc_page(void)
+{
+        return NULL;
+}
+
+void *
+alloc_pages(size_t cnt)
+{
+        return NULL;
+}
+
+void
+free_page(const void *page)
+{
+}
+
+void
+free_pages(const void *page, size_t cnt)
+{
+}
